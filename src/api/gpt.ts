44bbc5a8ce488f6511c13c3b96ca3d660a425afe
itttm127 <187483125+itttm127@users.noreply.github.com>
2025-10-16 23:05:28 +0900

    feat: update env on frontend

--- src/api/gpt.ts
+++ src/api/gpt.ts
@@ -1,5 +1,6 @@
 import OpenAI from "openai";
-const client = new OpenAI({dangerouslyAllowBrowser: true, apiKey: process.env.OPENAI_API_KEY});
+
+const client = new OpenAI({dangerouslyAllowBrowser: true, apiKey: process.env.REACT_APP_OPENAI_API_KEY});
 
 export const sendToGPT = async function(req: string) {
     const response = await client.responses.create({
